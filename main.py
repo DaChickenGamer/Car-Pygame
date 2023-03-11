df16cdd4d8c1c3f819f5413141cb071e88e67592
Nihitp123 <125763327+Nihitp123@users.noreply.github.com>
2023-03-11 13:42:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,88 @@
-import pygame, sys
-from pygame.locals import QUIT
+import pygame
+import random
+
 pygame.init()
-x_pos=400 
-y_pos=400
-x=2000
-y=2000
-screen = pygame.display.set_mode((x,y))
-class PlayerCar(pygame.sprite.Sprite):
-  def __init__(self,car_image,x_pos,y_pos):
-    self.car_image=car_image
-    self.x_pos=x_pos
-    self.y_pos=y_pos
-  def draw (self,screen):
-    screen.blit(self.car_image,(self.x_pos,self.y_pos))
-  def move_left(self):
-    self.x_pos-=5
-  def move_right(self):
-    self.x_pos+=5
-pygame.display.set_caption('start game!')
-car_texture=pygame.image.load("images (5).jpeg")
-backround=pygame.image.load("download (5).jpeg")
-backround=pygame.transform.scale(backround,(900,900))
-screen.blit(backround,(0,0))
-car_surface=pygame.Surface((car_texture.get_width(),car_texture.get_height()))
-car_surface.blit(car_texture,(0,0))
-player_car=PlayerCar(car_surface,x_pos, y_pos)
-player_car.draw(screen)
-running=True
-while running :
-    for event in pygame.event.get():  
-        if event.type == QUIT:
-          running=False
-        elif event.type==pygame.KEYDOWN:
-          if event.type==pygame.K_LEFT:
-            player_car.move_left()
-          if event.type==pygame.K_RIGHT:
-            player_car.move_right()
-    screen.fill((0,0,0))
-    player_car.draw(screen)
-    pygame.display.flip()
+
+# Set up the game window
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 800
+game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Racing Game")
+
+# Set up the colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+# Set up the player car
+player_car_image = pygame.image.load("images (5).jpeg")
+player_car_x = 250
+player_car_y = 600
+player_car_speed = 0
+
+# Set up the enemy cars
+enemy_car_images = [pygame.image.load("download.png"), pygame.image.load("images (5).jpeg"), pygame.image.load("download.png")]
+enemy_cars = []
+enemy_car_speed = 5
+enemy_car_spawn_timer = 0
+
+# Set up the game loop
+game_running = True
+clock = pygame.time.Clock()
+
+while game_running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_car_speed = -5
+            if event.key == pygame.K_RIGHT:
+                player_car_speed = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_car_speed = 0
+
+    # Update player car position
+    player_car_x += player_car_speed
+    if player_car_x < 0:
+        player_car_x = 0
+    if player_car_x > WINDOW_WIDTH - player_car_image.get_width():
+        player_car_x = WINDOW_WIDTH - player_car_image.get_width()
+
+    # Spawn enemy cars
+    enemy_car_spawn_timer += 1
+    if enemy_car_spawn_timer >= 60:
+        enemy_car_spawn_timer = 0
+        enemy_car_image = random.choice(enemy_car_images)
+        enemy_car_x = random.randint(0, WINDOW_WIDTH - enemy_car_image.get_width())
+        enemy_car_y = -enemy_car_image.get_height()
+        enemy_cars.append((enemy_car_image, enemy_car_x, enemy_car_y))
+
+    # Update enemy car positions
+    for i in range(len(enemy_cars)):
+        enemy_cars[i] = (enemy_cars[i][0], enemy_cars[i][1], enemy_cars[i][2] + enemy_car_speed)
+
+    # Remove off-screen enemy cars
+    enemy_cars = [enemy_car for enemy_car in enemy_cars if enemy_car[2] < WINDOW_HEIGHT]
+
+    # Check for collisions
+    for enemy_car in enemy_cars:
+        enemy_car_rect = pygame.Rect(enemy_car[1], enemy_car[2], enemy_car[0].get_width(), enemy_car[0].get_height())
+        player_car_rect = pygame.Rect(player_car_x, player_car_y, player_car_image.get_width(), player_car_image.get_height())
+        if enemy_car_rect.colliderect(player_car_rect):
+            game_running = False
+
+    # Draw the game
+    game_window.fill(WHITE)
+    game_window.blit(player_car_image, (player_car_x, player_car_y))
+    for enemy_car in enemy_cars:
+        game_window.blit(enemy_car[0], (enemy_car[1], enemy_car[2]))
     pygame.display.update()
+
+    # Set the game clock
+    clock.tick(60)
+
+# Clean up the game
 pygame.quit()
-sys.exit()
